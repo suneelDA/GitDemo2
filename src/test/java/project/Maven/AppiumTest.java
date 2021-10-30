47c280e1cd59d82c260f925bdaa82c48d1901ec7
suneelratnala123@gmail.com <suneelratnala123@gmail.com>
2021-10-30 11:45:03 +0530

    i am in gitone

--- src/test/java/project/Maven/AppiumTest.java
+++ src/test/java/project/Maven/AppiumTest.java
@@ -9,12 +9,15 @@ public class AppiumTest {
 	public void appAndroid()
 	{
 		System.out.println("appAndroid");
+		//new 
+		System.out.println("aaa");
 	}
 	
 	@Test
 	public void IOSapps()
 	{
 		System.out.println("IOSapps");
+		System.out.println("qqq");
 	}
 
 
